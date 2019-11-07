54f027d6f7281dad8f555c27f5b5af6c80c8e5b3
Den Pal <denispal@gmail.com>
2019-11-07 18:06:31 +0300

    file deploy_docker double qutes

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,6 +7,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker build -f Dockerfile -t "${TRAVIS_REPO_SLUG}":"${TAG}" .
+docker push "${TRAVIS_REPO_SLUG}"
 
