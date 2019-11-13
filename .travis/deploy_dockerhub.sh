7f2aec4690a2010a24e18989b57699ffb719f944
Den Pal <denispal@gmail.com>
2019-11-13 15:07:18 +0300

    v 1.2

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-#docker login -u $DOCKER_USER -p $DOCKER_PASS
+#docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-REPONAME="hoper/cicd-buzz"
-docker build -f Dockerfile -t "$REPONAME":"$TAG" .
+#REPONAME="hoper/cicd-buzz"
+#docker build -f Dockerfile -t "$REPONAME":"$TAG" .
+docker build -f Dockerfile -t "$DOCKER_USER/cicd-buzz":"$TAG" .
 #docker push "$TRAVIS_REPO_SLUG"
-docker push "$REPONAME":"$TAG"
-
+#docker push "$REPONAME":"$TAG"
+docker push "$DOCKER_USER/cicd-buzz":"$TAG"
